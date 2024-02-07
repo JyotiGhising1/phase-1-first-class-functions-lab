2a84e0b4274491f4b8a0c8a6b0d18afb7f930874
JyotiGhising1 <83853614+JyotiGhising1@users.noreply.github.com>
2024-02-07 15:16:10 -0500

    Completed First Class Function Lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+const selectDifferentDrivers = function(arrayOfDrivers, selectingDrivers){
+    return selectingDrivers(arrayOfDrivers);
+}
+
+const nameOfDrivers=['Antonia', 'Nuru', 'Amari', 'Mo'];
+const firstTwoDriver = selectDifferentDrivers(nameOfDrivers, returnFirstTwoDrivers);
+const lastTwoDrivers = selectDifferentDrivers(nameOfDrivers,returnLastTwoDrivers);
+
+const createFareMultiplier = function(fareMultiplier){
+    return function(value){
+        return value * fareMultiplier;
+    } 
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
